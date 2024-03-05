57e0951e970d18049c6fc351397208ad396f636c
LynaSovann <sovannlyna2004@gmail.com>
2024-03-05 15:36:41 +0700

    ..

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ import org.example.service.serviceImpl.ProductServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        ProductServiceImpl.getAllProducts();
+
     }
 }
