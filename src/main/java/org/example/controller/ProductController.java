f52094a60396b507125674b95453f2df2342c4c5
LynaSovann <sovannlyna2004@gmail.com>
2024-03-07 21:59:22 +0700

    update savedProduct

--- src/main/java/org/example/controller/ProductController.java
+++ src/main/java/org/example/controller/ProductController.java
@@ -13,7 +13,7 @@ public class ProductController {
     }
 
     public void savedProduct(){
-        productView.displaySavedCase(productService.savedProduct(), productService.saveUpdateProduct());
+        productView.displaySavedCase(productService.savedProduct(), productService.displayUpdatedProduct());
     }
 
     public void displayUnsavedProduct() {
